d31cdd7a73ca3dab1e6f58263310c282c6a24a12
Fauzi Nur Noviansyah <fnnoviansyah@gmail.com>
2022-03-19 19:16:24 +0700

    revert path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:url_strategy/url_strategy.dart';
 import 'package:yuk_vaksin_web/features/article/view/article_binding.dart';
 import 'package:yuk_vaksin_web/features/dashboard/view/dashboard_binding.dart';
 import 'package:yuk_vaksin_web/features/home/view/home_binding.dart';
@@ -26,7 +25,6 @@ import 'features/vaccineplace/view/vaccine_place_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
   initializeDateFormatting();
   MainBinding().dependencies();
   // HttpOverrides.global = MyHttpoverrides();
